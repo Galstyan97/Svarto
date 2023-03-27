e6636ec34ac34c7a7a49cae4a4eab42c4bd2e201
Galstyan <Galstyan@HP-Loptop>
2023-03-27 20:54:13 +0400

    Corrected argument name in binary search

--- Search_Algorithms/binary_search18.03.23.py
+++ Search_Algorithms/binary_search18.03.23.py
@@ -1,9 +1,9 @@
-def binary_search(array, x, low, high): 
+def binary_search(array, searching_element, low, high): 
     while low <= high:
         mid = low + (high - low) // 2
-        if array[mid] == x:
+        if array[mid] == searching_element:
             return mid
-        elif array[mid] < x:
+        elif array[mid] < searching_element:
             low = mid + 1
         else:
             high = mid - 1
@@ -11,10 +11,10 @@ def binary_search(array, x, low, high):
     return - 1
 
 array = [2,4,7,8,9,10]
-x = 9
+searching_element = 9
 low = 0
 high = len(array) - 1
-result = binary_search(array, x, low, high)
+result = binary_search(array, searching_element, low, high)
 if result == -1:
     print("not found")
 else:
